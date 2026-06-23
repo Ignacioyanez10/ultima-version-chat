7bfebfbea1fc4c4e4f6a2b615198fd07f4a5a977
Ignacio Yañez <ignacioyz.perez@gmail.com>
2026-06-23 01:58:26 -0400

    fix: usar process.env.PORT para Railway

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ io.on('connection', (socket) => {
 });
 
 // Iniciamos el servidor localmente en el puerto 3000
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Servidor corriendo localmente en: http://localhost:${PORT}`);
 });
